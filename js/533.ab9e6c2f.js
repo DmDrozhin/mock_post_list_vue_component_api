7b79854babd0dbd6b031dba207e0393609e1ea6c
Dmitriy Drozhin <ddrozhin@gmail.com>
2023-11-08 21:50:32 +0200

    deleted:    dist/css/app.16f77c36.css 	new file:   dist/css/app.37fef49c.css 	modified:   dist/index.html 	renamed:    dist/js/533.347cba69.js -> dist/js/533.ab9e6c2f.js 	renamed:    dist/js/533.347cba69.js.map -> dist/js/533.ab9e6c2f.js.map 	renamed:    dist/js/890.ecf78ff4.js -> dist/js/890.29eae151.js 	renamed:    dist/js/890.ecf78ff4.js.map -> dist/js/890.29eae151.js.map 	new file:   dist/js/896.b179a9e4.js 	new file:   dist/js/896.b179a9e4.js.map 	new file:   dist/js/app.e2d2a3c9.js 	new file:   dist/js/app.e2d2a3c9.js.map 	deleted:    dist/js/app.e30bf46c.js 	deleted:    dist/js/app.e30bf46c.js.map 	renamed:    dist/js/chunk-vendors.dbc11c96.js -> dist/js/chunk-vendors.cee29a3d.js 	new file:   dist/js/chunk-vendors.cee29a3d.js.map 	deleted:    dist/js/chunk-vendors.dbc11c96.js.map 	modified:   src/App.vue 	modified:   src/components/PostList.vue 	new file:   src/directives/VIntersection.js 	new file:   src/directives/index.js 	modified:   src/main.js 	modified:   src/pages/MainPage.vue 	new file:   src/pages/SinglePostPage.vue 	modified:   src/router/index.js 	modified:   src/store/index.js 	modified:   src/styles/style.css 	modified:   vue.config.js

--- js/533.ab9e6c2f.js
+++ js/533.ab9e6c2f.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunk_2023_10_26_vue3_mock_project"]=self["webpackChunk_2023_10_26_vue3_mock_project"]||[]).push([[533],{8533:function(t,e,n){n.r(e),n.d(e,{default:function(){return s}});var r=n(3396),u=n(7139);const c={class:"container"};function _(t,e,n,_,a,o){return(0,r.wg)(),(0,r.iD)("div",c,[(0,r._)("h1",null,(0,u.zw)(a.title),1)])}var a={name:"AboutPage",data(){return{title:"About"}}},o=n(89);const i=(0,o.Z)(a,[["render",_]]);var s=i}}]);
-//# sourceMappingURL=533.347cba69.js.map
+//# sourceMappingURL=533.ab9e6c2f.js.map
---
+++ js/896.b179a9e4.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunk_2023_10_26_vue3_mock_project"]=self["webpackChunk_2023_10_26_vue3_mock_project"]||[]).push([[896],{896:function(t,e,r){r.r(e),r.d(e,{default:function(){return i}});var n=r(3396),u=r(7139);const c={class:"container"};function o(t,e,r,o,s,_){return(0,n.wg)(),(0,n.iD)("div",c,[(0,n._)("h1",null,(0,u.zw)(s.title)+" : "+(0,u.zw)(_.postId),1)])}var s={data(){return{title:"Single post"}},computed:{postId(){return this.$route.params.id}}},_=r(89);const a=(0,_.Z)(s,[["render",o]]);var i=a}}]);
+//# sourceMappingURL=896.b179a9e4.js.map
